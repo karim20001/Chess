eb670a34b4dd9dbbb9bacd5e212f1bed3ee9df0c
ali2002kh <73595075+ali2002kh@users.noreply.github.com>
2021-12-16 15:38:30 +0330

    massage

---
+++ Ds-classes/stack.js
@@ -0,0 +1,42 @@
+class Stack {
+    constructor() {
+        this.top = 0;
+        this.elements = [];
+    }
+
+    empty () {
+        if (this.top === 0) {
+            return true;
+        } 
+        return false;
+    }
+
+    push (x) {
+        this.elements[this.top] = x;
+        this.top++;
+    }
+
+    pop () {
+        if (this.top === 0) {
+            return undefined;
+        }
+        this.top--;
+        var y = this.elements[this.top];
+        // delete this.elements[this.top];
+        return y;
+    }
+
+    peek () {
+        if (this.top === 0) {
+            return undefined;
+        }
+        var y = this.elements[this.top-1];
+        return y;
+    }
+
+    size () {
+        return this.top;
+    }
+}
+
+
